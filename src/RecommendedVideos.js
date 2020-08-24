7ff89d27b0d7d8495b507afcdb946e2e60d42b26
Sudhirsin <lion.sud.k@gmail.com>
2020-08-25 00:21:06 +0530

    Sidebar component completed

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RecommendedVideos.css';
+
+function RecommendedVideos() {
+    return (
+        <div className="recommendedVideos">
+            Recommeded 
+        </div>
+    )
+}
+
+export default RecommendedVideos;
