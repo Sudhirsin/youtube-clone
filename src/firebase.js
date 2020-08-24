494473a06304181af801395d68e721a0fcf518c2
Sudhirsin <lion.sud.k@gmail.com>
2020-08-24 21:48:14 +0530

    App Clean up done

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCspgZ0f7WbdCuUbZjuYGd8MsDQnzBrYZk",
+    authDomain: "clone-a2cf1.firebaseapp.com",
+    databaseURL: "https://clone-a2cf1.firebaseio.com",
+    projectId: "clone-a2cf1",
+    storageBucket: "clone-a2cf1.appspot.com",
+    messagingSenderId: "355667459256",
+    appId: "1:355667459256:web:4347a487ef79b4d08d484d",
+    measurementId: "G-ZPH0B2NWCJ"
+  };
