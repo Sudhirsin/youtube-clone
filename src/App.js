21550afcbbd882d2cb37e8a331b0ed1d084c2b9c
Sudhirsin <lion.sud.k@gmail.com>
2020-08-24 22:48:37 +0530

    Header componet completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
-      Youtube clone
 
       {/* Header */}
+      <Header />
 
       {/* Sidebar */}
 
       {/* Recomended Video section */}
-      
+
     </div>
   );
 }
